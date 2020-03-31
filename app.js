b2115ec8f930c2ba30caed7240e8ea1ab01f1d2d
Jacqueline Remmel <jpremmel@gmail.com>
2020-03-31 14:13:47 -0700

    Switch from manually creating deferred object to using  function

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 //"A deferred object is simply an object that exposes a promise as well as the associated methods for resolving that promise. It is constructed using the $q.deferred() function and exposes three main methods: resolve(), reject(), and notify(). The associated promise object can be accessed via the promise property."
 
-//"Here we create a new deferred object, then return its promise property. We also execute our asnyc function and after it completes, we resolve the deferred object. The parameter of the resolve() function will be passed to the callback function."
 function getData($timeout, $q) {
   return function() {
-    var defer = $q.defer();
+
+    //OPTION 1: MANUALLY CREATE DEFERRED OBJECT
+    // var defer = $q.defer();
 
     //simulated async function
-    $timeout(function() {
-      if(Math.round(Math.random())) {
-        defer.resolve('data received!');
-      } else {
-        defer.reject('oh no an error! try again.');
-      }
-    }, 2000);
+    // $timeout(function() {
+    //   if(Math.round(Math.random())) {
+    //     defer.resolve('data received!');
+    //   } else {
+    //     defer.reject('oh no an error! try again.');
+    //   }
+    // }, 2000);
 
-    return defer.promise;
+    //return defer.promise;
+
+    //OPTION 2: USE $q FUNCTION TO DO THE SAME THING
+    return $q(function(resolve, reject) {
+      $timeout(function() {
+        if (Math.round(Math.random())) {
+          resolve('data received!');
+        } else {
+          reject('oh no an error! try again.');
+        }
+      }, 2000);
+    });
+
   };
 }
 
