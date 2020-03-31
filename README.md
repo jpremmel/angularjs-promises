a6b6005569cc824de36eab86480a1028884622ba
Jacqueline Remmel <jpremmel@gmail.com>
2020-03-31 13:56:34 -0700

    Create deferred object and return its promise property

---
+++ README.md
@@ -0,0 +1,3 @@
+Learning AngularJS with https://thinkster.io/a-better-way-to-learn-angularjs
+
+Promises lesson: https://thinkster.io/a-better-way-to-learn-angularjs/promises
---
+++ app.js
@@ -0,0 +1,21 @@
+//"A deferred object is simply an object that exposes a promise as well as the associated methods for resolving that promise. It is constructed using the $q.deferred() function and exposes three main methods: resolve(), reject(), and notify(). The associated promise object can be accessed via the promise property."
+
+//"Here we create a new deferred object, then return its promise property. We also execute our asnyc function and after it completes, we resolve the deferred object. The parameter of the resolve() function will be passed to the callback function."
+function getData($timeout, $q) {
+  return function() {
+    var defer = $q.defer();
+
+    //simulated async function
+    $timeout(function() {
+      defer.resolve('data received!');
+    }, 2000);
+
+    return defer.promise;
+  };
+}
+
+angular.module('app', [])
+.factory('getData', getData)
+.run(function(getData) {
+  var promise = getData();
+});
