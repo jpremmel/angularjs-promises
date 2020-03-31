5e82433e2192ca444f6db66126a5524376cc62b2
Jacqueline Remmel <jpremmel@gmail.com>
2020-03-31 14:25:57 -0700

    Chain promises

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
-//"A deferred object is simply an object that exposes a promise as well as the associated methods for resolving that promise. It is constructed using the $q.deferred() function and exposes three main methods: resolve(), reject(), and notify(). The associated promise object can be accessed via the promise property."
-
 function getData($timeout, $q) {
   return function() {
 
-    //OPTION 1: MANUALLY CREATE DEFERRED OBJECT
-    // var defer = $q.defer();
-
     //simulated async function
-    // $timeout(function() {
-    //   if(Math.round(Math.random())) {
-    //     defer.resolve('data received!');
-    //   } else {
-    //     defer.reject('oh no an error! try again.');
-    //   }
-    // }, 2000);
-
-    //return defer.promise;
-
-    //OPTION 2: USE $q FUNCTION TO DO THE SAME THING
     return $q(function(resolve, reject) {
       $timeout(function() {
-        if (Math.round(Math.random())) {
-          resolve('data received!');
-        } else {
-          reject('oh no an error! try again.');
-        }
+        resolve(Math.floor(Math.random() * 10));
       }, 2000);
     });
 
@@ -35,12 +15,11 @@ angular.module('app', [])
 .factory('getData', getData)
 .run(function(getData) {
   var promise = getData()
-  .then(function(string) {
-    console.log(string);
-  }, function(error) {
-    console.log(error);
+  .then(function(num) {
+    console.log(num);
+    return num * 2;
   })
-  .finally(function() { //either 'resolve' or 'reject' is called; regardless of which one, 'finally' is called afterward
-    console.log('Finished at: ', new Date());
+  .then(function(num) {
+    console.log(num); // = random number * 2
   });
 });
